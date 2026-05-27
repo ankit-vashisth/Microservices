3982a8ad4b85433b0990d69c4926eef22c99ed6a
Ankit Sharma <ankit07v@gmail.com>
2026-05-27 18:11:56 +0530

    Basic spring security added

--- USERSERVICE/src/main/java/com/example/services/userservices/securityconfiguration/SecurityConfig.java
+++ USERSERVICE/src/main/java/com/example/services/userservices/securityconfiguration/SecurityConfig.java
@@ -4,22 +4,28 @@
  */
 package com.example.services.userservices.securityconfiguration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  *
  * @author CCS
  */
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public org.springframework.security.web.SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth.requestMatchers("/userController/**").permitAll()
-//                .anyRequest().authenticated())
-//                .httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                //.requestMatchers("/userController/**").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }
